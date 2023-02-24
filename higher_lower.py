040fab0462d48541bcf4f800deef615f7e585227
Michael Primavera <pastasauce59@gmail.com>
2023-02-24 15:55:36 -0500

    created compare function to compare which of a or b has more followers, created guess input to ask the user to guess, created if statement to see if the user score's or loses the game.

--- higher_lower.py
+++ higher_lower.py
@@ -11,6 +11,14 @@ def random_selection():
     person = data[index]
     return [index, person]
 
+def compare(a, b):
+    if a[1]['follower_count'] > b[1]['follower_count']:
+        data.pop(b[0])
+        return 'a'
+    else:
+        data.pop[a[0]]
+        return 'b'
+
 score = 0
 a = random_selection()
 b = random_selection()
@@ -18,13 +26,19 @@ b = random_selection()
 while b == a:
     b = random_selection()
 
+
 print(f"Compare A: {a[1]['name']}, a {a[1]['description']}, from {a[1]['country']}.")
 
 print(vs)
 
 print(f"Compare B: {b[1]['name']}, a {b[1]['description']}, from {b[1]['country']}.")
 
-print("Who has more followers? Type 'A' or 'B': ")
+answer = compare(a,b)
+print(f" pssst, the answer is {answer}.")
 
-# print(data.pop())
-# print(data)
+guess = input("Who has more followers? Type 'A' or 'B': ")
+if guess == answer:
+    score += 1
+    print(f"You're right! Current score: {score}")
+else:
+    print(f"Sorry, that's wrong. Final score: {score}")
