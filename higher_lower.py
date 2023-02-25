df4e752cea2a78e02c97c333a3a6df2530680b44
Michael Primavera <pastasauce59@gmail.com>
2023-02-25 18:26:08 -0500

    created 'game' function that houses the score and while loop that runs the code of the higher-lower game, added documentation statements to functions.

--- higher_lower.py
+++ higher_lower.py
@@ -3,19 +3,18 @@ from art import logo, vs
 from game_data import data
 from random import randint
 
-print(logo)
 
 def random_selection():
     """Gets a random number from randint and returns the number as an index and person from 'data' list."""
     index = randint(1, len(data) - 1)
     person = data[index]
-    # return [index, person]
     return {
         'index': index,
         'person': person
     }
 
 def compare(a, b):
+    """Compares the follower account of dict a and dict b and returns the higher dict with updated answer."""
     if a['person']['follower_count'] > b['person']['follower_count']:
         data.pop(b['index'])
         a.update({'answer': 'a'})
@@ -25,35 +24,38 @@ def compare(a, b):
         b.update({'answer': 'b'})
         return b
 
-score = 0
-a = random_selection()
+def game():
+    """Runs the flow of code for higher-lower game. Game continues until user gets the answer wrong."""
+    score = 0
+    a = random_selection()
 
-keep_playing = True
-while keep_playing:
-    b = random_selection()
-    #To ensure 'a' and 'b' never wind up being the same random selection
-    while b == a:
+    keep_playing = True
+    while keep_playing:
+        print(logo)
         b = random_selection()
+        #To ensure 'a' and 'b' never wind up being the same random selection
+        while b == a:
+            b = random_selection()
 
+        print(f"Compare A: {a['person']['name']}, a {a['person']['description']}, from {a['person']['country']}.")
 
-    # print(f"Compare A: {a[1]['name']}, a {a[1]['description']}, from {a[1]['country']}.")
-    print(f"Compare A: {a['person']['name']}, a {a['person']['description']}, from {a['person']['country']}.")
+        print(vs)
 
-    print(vs)
+        print(f"Compare B: {b['person']['name']}, a {b['person']['description']}, from {b['person']['country']}.")
 
-    # print(f"Compare B: {b[1]['name']}, a {b[1]['description']}, from {b[1]['country']}.")
-    print(f"Compare B: {b['person']['name']}, a {b['person']['description']}, from {b['person']['country']}.")
+        answer = compare(a,b)
+        #FOR TESTING
+        # print(f" pssst, the answer is {answer['answer']}.")
 
-    answer = compare(a,b)
-    print(f" pssst, the answer is {answer['answer']}.")
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        if guess == answer['answer']:
+            import os
+            os.system('clear')
+            score += 1
+            a = answer
+            print(f"You're right! Current score: {score}")
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}")
+            keep_playing = False
 
-    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
-    if guess == answer['answer']:
-        import os
-        os.system('clear')
-        score += 1
-        a = answer
-        print(f"You're right! Current score: {score}")
-    else:
-        print(f"Sorry, that's wrong. Final score: {score}")
-        keep_playing = False
+game()
