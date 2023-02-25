5b69a4628f3da03f9356a0f20fdf375cef32c67b
Michael Primavera <pastasauce59@gmail.com>
2023-02-25 17:42:12 -0500

    changed the random_selection function to return a list with key and value pairs instead of an array.

--- higher_lower.py
+++ higher_lower.py
@@ -9,14 +9,18 @@ def random_selection():
     """Gets a random number from randint and returns the number as an index and person from 'data' list."""
     index = randint(1, len(data) - 1)
     person = data[index]
-    return [index, person]
+    # return [index, person]
+    return {
+        'index': index,
+        'person': person
+    }
 
 def compare(a, b):
-    if a[1]['follower_count'] > b[1]['follower_count']:
-        data.pop(b[0])
+    if a['person']['follower_count'] > b['person']['follower_count']:
+        data.pop(b['index'])
         return 'a'
     else:
-        data.pop[a[0]]
+        data.pop(a['index'])
         return 'b'
 
 score = 0
@@ -27,16 +31,18 @@ while b == a:
     b = random_selection()
 
 
-print(f"Compare A: {a[1]['name']}, a {a[1]['description']}, from {a[1]['country']}.")
+# print(f"Compare A: {a[1]['name']}, a {a[1]['description']}, from {a[1]['country']}.")
+print(f"Compare A: {a['person']['name']}, a {a['person']['description']}, from {a['person']['country']}.")
 
 print(vs)
 
-print(f"Compare B: {b[1]['name']}, a {b[1]['description']}, from {b[1]['country']}.")
+# print(f"Compare B: {b[1]['name']}, a {b[1]['description']}, from {b[1]['country']}.")
+print(f"Compare B: {b['person']['name']}, a {b['person']['description']}, from {b['person']['country']}.")
 
 answer = compare(a,b)
 print(f" pssst, the answer is {answer}.")
 
-guess = input("Who has more followers? Type 'A' or 'B': ")
+guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 if guess == answer:
     score += 1
     print(f"You're right! Current score: {score}")
